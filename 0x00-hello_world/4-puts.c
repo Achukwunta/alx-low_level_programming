e6783f681108619187afd79724e0d95fdb2f5c4d
Augustine Chukwunta <augustine.chukwunta@gmail.com>
2023-02-18 02:43:51 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -C program that prints using a pus funcion
+ * Reurn - is always 0 means success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
