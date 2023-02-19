ecc0bfd9bf3770ea8462ade66d61751f076b0eac
Augustine Chukwunta <augustine.chukwunta@gmail.com>
2023-02-19 14:54:36 -0800

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,17 +4,16 @@
 
 /**
  * main - C  program that prints all single digit number
- * of base 10 starting from 0
- *  followed by a new line.
- *  Return: 0 (success)
+ * followed by a new line.
+ * Return: 0 (success)
  */
 
 int main(void)
 {
 	int n;
 
-	for (n = 0 ; n < 10 ; n++)
-		putchar(n + '0');
+	for (n = '0' ; n <= '9' ; n++)
+		putchar(n);
 	putchar('\n');
 	return (0);
 }
