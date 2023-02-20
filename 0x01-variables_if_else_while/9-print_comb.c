704d55f2c10851b59b4e9e01f9f6d7166faabd48
Augustine Chukwunta <augustine.chukwunta@gmail.com>
2023-02-20 12:56:58 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	int n;
 
-	for (n = 0 ; n < 10 ; n++)
-		putchar(n + '0');
-	if (n < 9)
-		putchar(',');
-	putchar(' ');
+	for (n = 48 ; n < 58 ; n++)
+		putchar(n);
+	if (n < 57)
+		putchar(44);
+	putchar(32);
 	putchar('\n');
 	return (0);
 }
