6f22c26d68281b456bbf904bbafe8ca6b1735a3f
Augustine Chukwunta <augustine.chukwunta@gmail.com>
2023-02-21 13:43:16 -0800

    10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet - a function that prints 10x the alphabet
+ * print_alphabet_x10 - a function that prints 10x the alphabet
  * in lowercase, followed by a new line.
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 char letter;
 
